66358bcbb31efd583943c848667a0da477559204
Paras Tomar <parastomar851@gmail.com>
2025-08-09 22:27:58 +0530

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,29 +1,47 @@
 const inputvalue = document.getElementById("inputvalue");
-const mybutton = document.querySelector('.todo-list-elem');
-
+const tasklist = document.getElementById("tasklist");
 let localtodolsit = [];
 
 const gettodolist = () => {
-     return JSON.parse(localStorage.getItem("ToDolist"))
-}
+    return JSON.parse(localStorage.getItem("ToDolist"));
+};
+
+const savetodolist = () => {
+    localStorage.setItem("ToDolist", JSON.stringify(localtodolsit));
+};
+
+const renderList = () => {
+    tasklist.innerHTML = "";
+    localtodolsit.forEach((task) => {
+        const divElement = document.createElement("div");
+        divElement.classList.add("main-todo-list");
+        divElement.innerHTML = `<li>${task}</li> <button class="deletebtn">Delete</button>`;
+
+         
+        divElement.querySelector(".deletebtn").addEventListener("click", () => {
+            localtodolsit = localtodolsit.filter(item => item !== task);
+            savetodolist();
+            renderList();
+        });
+
+        tasklist.append(divElement);
+    });
+};
 
 const addtodolist = () => {
-
     const todolistvalue = inputvalue.value.trim();
+    if (!todolistvalue) return;
 
     localtodolsit = gettodolist() || [];
-
     localtodolsit.push(todolistvalue);
-    localtodolsit = [...new Set(localtodolsit)] 
-    console.log(localtodolsit)
-    localStorage.setItem("ToDolist", JSON.stringify(localtodolsit))
+    localtodolsit = [...new Set(localtodolsit)];
+    savetodolist();
+    renderList();
+    inputvalue.value = "";
+};
 
-   const divElement = document.createElement("div")
-   divElement.classList.add("main-todo-list")
-   divElement.innerHTML = `<li>${inputvalue.value}</li> <button class="deletebtn">Delete</button>`
-   tasklist.append(divElement)
-}
+document.querySelector('.btn1').addEventListener('click', addtodolist);
 
-document.querySelector('.btn1').addEventListener('click', () => {
-    addtodolist();
-})
+// Load todos on page load
+localtodolsit = gettodolist() || [];
+renderList();
